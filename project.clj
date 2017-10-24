0641e688db838a6a6fb59e43138c06c4a51a2e76
Ben Oliver <benjamin.e.oliver@gmail.com>
2017-10-24 08:58:44 +0200

    project file fun

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject beoliver/swap-and-return "0.1.0-SNAPSHOT"
+(defproject beoliver/swap-and-return "0.1.0"
   :description "A simple library for updating atoms and retrning values"
   :url "https://github.com/beoliver/swap-and-return"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :aot [swap-and-return.core]
+  :deploy-repositories [["clojars" {:sign-releases false}]])
